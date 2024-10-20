cfe2f2b4c4fa1cf4c183fc163242b00ea5691ce3
Reut Mihai <reutmihai@icloud.com>
2024-10-20 16:27:59 +0300

    Updates

---
+++ precache-manifest.f5171460fa3bf5a7a8c918c6453699db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b78a2ca80688d5613ea692078b9e191",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "406beeb7d664b555a1d9",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.9f8d5030.chunk.css"
+  },
+  {
+    "revision": "1a1dd193c9b832bcd485",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.c282ec78.chunk.js"
+  },
+  {
+    "revision": "406beeb7d664b555a1d9",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.c4af8914.chunk.js"
+  },
+  {
+    "revision": "052338c15f0cfa810fb3",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime~main.a33a0e9d.js"
+  }
+]);
